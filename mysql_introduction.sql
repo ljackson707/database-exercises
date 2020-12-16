007736bc6b077aa4b8e74d0c73fd50758df0135c
Liam Jackson <ljackson3613@yahoo.com>
2020-12-16 11:01:53 -0600

    Notes on SQL Addition

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -4,9 +4,23 @@ SELECT * FROM mysql.user;
 # Only showing user and host columns
 SELECT user, host FROM mysql.user;
 
-#All the rows beacuse we use * from the help_topic table of the MySQL database
+# All the rows beacuse we use * from the help_topic table of the MySQL database
 SELECT * FROM mysql.help_topic;
 
 # Selecting only the help_topic_id and help_category_id column from mysql.help_topic
 SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
+
+# Kinda like ls for all databases
+show databases;
+
+# Sets our current databse to fruits.db
+USE fruits_db;
+
+# Show the currently selected DB, kinda like pwd
+SELECT databases();
+
+# Show the code that created this specific database
+SHOW CREATE DATABASE fruits_db;
+
+# 
 
