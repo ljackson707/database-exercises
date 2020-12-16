ad8a1f436109321075fb21782f31a5b152bde206
Liam Jackson <ljackson3613@yahoo.com>
2020-12-16 10:43:51 -0600

    add first exercise and repeat

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -1,8 +1,12 @@
+# * Means all columns from a database file
 SELECT * FROM mysql.user;
 
+# Only showing user and host columns
 SELECT user, host FROM mysql.user;
 
+#All the rows beacuse we use * from the help_topic table of the MySQL database
 SELECT * FROM mysql.help_topic;
 
+#
 SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
 
