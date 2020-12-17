0dfb6a48bf99817ba9212f36608c823bb628a55d
Liam Jackson <ljackson3613@yahoo.com>
2020-12-17 09:35:16 -0600

    changes made to relationship question

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -16,6 +16,9 @@ SHOW CREATE TABLE employees;
 #-hire_date
 #-birth_date
 
-# The employees table has a emp_no column that refrences the emp_no on the departments tables
+# The departments table does not have a direct (do not share keys) relationship with employees table. There is insigt found through exploration when looking at dept.emp table which had two forigne keys, one to point to a dept_no and another to point to an emp_no 
+	#-This is called a many to many relationship. 
+	#(Sign1 = 2 unrealted tables and a third that relates the two tables) 
+	#(Sign2 = Can more than one employee or departments hav more than one employee or department associated with it?)
 
 SHOW CREATE TABLE dept_manager;
