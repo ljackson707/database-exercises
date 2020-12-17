1db0187e5ae8934e5fffb08797ae3ebb099aa215
Liam Jackson <ljackson3613@yahoo.com>
2020-12-17 13:57:43 -0600

    select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,18 +1,30 @@
 use albums_db;
 
 #Explore Structure of Albums
+Describe albums;
 
 #Explore content
+SELECT *
+FROM albums;
 
-#The name of all albums by Pink Floyd
+#Explore Distinct column
+SELECT DISTINCT genre
+FROM albums;
+
+#The name of all albums by Pink Floyd (With Alias)
 SELECT name , artist
 FROM albums
 WHERE artist = 'Pink Floyd';
 
-#The year Sgt. Pepper's Lonely Hearts Club Band was released
+#ALias
+SELECT name AS 'Albums by Pink Floyd'
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+#The year Sgt. Pepper's Lonely Hearts Club Band was released (Use backslash to opt out the ')
 SELECT name , release_date
 FROM albums
-WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
+WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 
 #The genre for the album Nevermind
 SELECT genre , name 
@@ -25,7 +37,7 @@ FROM albums
 WHERE release_date between 1990 and 1999;
 
 #Which albums had less than 20 million certified sales
-SHOW CREATE TABLE albums;
+SHOW CREATE TABLE albums; #To check the format of the sales column.
 SELECT name , sales
 FROM albums
 WHERE sales < 20.0;
@@ -35,10 +47,11 @@ SHOW CREATE TABLE albums;
 SELECT *
 FROM albums
 WHERE genre = 'Rock'; 
+
 #There is a genre of just "Rock". If we wanted to ve it include all rock genres then we would use a concat and alias. 
 
 # Or you can use the LIKE operand
 SHOW CREATE TABLE albums;
 SELECT *
 FROM albums
-WHERE genre LIKE '%Rock%';
+WHERE genre LIKE '%Rock,%';
