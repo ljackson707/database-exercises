e11dd51d7e9094d132396842249759206a2062e7
Liam Jackson <ljackson3613@yahoo.com>
2020-12-18 15:26:36 -0600

    -

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -149,13 +149,16 @@ WHERE emp_no between 101010 and 202020
 AND last_name LIKE "%q%"
 ORDER BY last_name ASC;
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #Exercises Functions 
 
 #1)Copy the order by exercise and save it as functions_exercises.sql.
 use employees;
 
+SELECT *
+FROM employees
+LIMIT 10;
 #2)Write a query to to find all current employees whose last name starts and ends with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
 SELECT concat(first_name," ", Last_name) as full_name
 FROM employees
@@ -167,7 +170,10 @@ FROM employees
 WHERE last_name LIKE 'e%e';
 
 #4)Find all previous employees hired in the 90s and born on Christmas. Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE()),
-SELECT datediff(NOW(), hire_date) AS how_many_days_at_company
+SELECT emp_no,
+		first_name,
+		last_name,
+		datediff(NOW(), hire_date) as how_many_days_at_company
 FROM employees
 WHERE hire_date LIKE '199%' 
 AND birth_date LIKE '%12-25';
@@ -182,9 +188,30 @@ SELECT max(salary) as max_current_salary
 FROM salaries
 WHERE to_date > CURDATE();#158220
 
-#6)Use your knowledge of built in SQL functions to generate a username for all of the current and previous employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
+#6)Use your knowledge of built in SQL functions to generate a username for all of the current and previous employees. 
 
-SELECT LOWER(CONCAT(SUBSTR(first_name,1,1),substr(last_name,1,4),"_",substr(birth_date,6,2),substr(birth_date,3,2)))
-FROM employees;
+#A username should be all lowercase, and consist of the first character of the employees first name, 
+
+#the first 4 characters of the employees last name, 
+
+#an underscore, 
+
+#the month the employee was born, 
+
+#and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
+
+SELECT lower(concat(substr(first_name,1, 1), 
+		substr(last_name, 1,4),
+		"_",
+		substr(birth_date,6,2),
+		substr(birth_date,3,2))) 
+		AS employee_username,
+		first_name AS first_name,
+		last_name AS last_name,
+		birth_date AS birth_date
+FROM employees
+WHERE birth_date LIKE '%12-25'
+ORDER BY first_name ASC
+LIMIT 20;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
