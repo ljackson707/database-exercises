f978612850f4bcce81a413f2809e7ca60a080d29
Liam Jackson <ljackson3613@yahoo.com>
2020-12-18 11:54:35 -0600

    -

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -48,6 +48,15 @@ SELECT DISTINCT title
 FROM titles
 LIMIT 10;
 
+Senior Engineer
+Staff
+Engineer
+Senior Staff
+Assistant Engineer
+Technique Leader
+Manager
+
+
 #3)Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
 SELECT *
 FROM employees
@@ -56,6 +65,12 @@ AND birth_date LIKE "%12-25"
 ORDER BY hire_date ASC
 LIMIT 5;
 
+243297	1962-12-25	Alselm	Cappello	F	1990-01-01
+34335	1960-12-25	Utz	Mandell	M	1990-01-03
+400710	1963-12-25	Bouchung	Schreiter	M	1990-01-04
+465730	1959-12-25	Baocai	Kushner	F	1990-01-05
+490744	1959-12-25	Petter	Stroustrup	M	1990-01-10
+
 #4)Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results.
 SELECT *
 FROM employees
@@ -63,6 +78,12 @@ WHERE hire_date LIKE "199%"
 AND birth_date LIKE "%12-25"
 ORDER BY hire_date ASC
 LIMIT 5 OFFSET 45;
+
+463558	1963-12-25	Pranay	Narwekar	F	1990-07-18
+444269	1963-12-25	Marjo	Farrow	F	1990-07-18
+291662	1962-12-25	Ennio	Karcich	M	1990-08-05
+413687	1954-12-25	Dines	Lubachevsky	F	1990-08-06
+416525	1952-12-25	Ipke	Fontan	M	1990-08-06
 
 #LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
 
